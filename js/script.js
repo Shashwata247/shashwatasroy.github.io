b32034a44ff7fc034ec79984cdb7bde230565b3d
SHASHWATA SOURAV ROY SAMYA <ssamya@smail.uni-koeln.de>
2025-08-15 16:38:44 +0200

    feat: add new portfolio site

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// Placeholder JavaScript file
+// Currently unused, but included for potential future enhancements such as
+// interactive features or dynamic content loading. You can add your own
+// functions here to enrich the user experience.
